e3b84b4501ec5ab381795d88c8aa9aa69e0ac4c3
AlxndrBo <sasha547@ya.ru>
2016-03-07 01:15:35 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+git config --global user.email "
+git config --global user.name "AlxndrBo"
 sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
