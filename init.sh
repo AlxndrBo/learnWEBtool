3d332de51dcc8f7a95a3c45a1f32bf5b59e92c9f
AlxndrBo <sasha547@ya.ru>
2016-03-07 17:54:37 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@ git config --global user.name "AlxndrBo"
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
-sudo ln -sf /home/box/web/hello.py  /usr/lib/python2.7/dist-packages/gunicorn/app/hello.py
-sudo /etc/init.d/gunicorn restart
+cd /home/box/web
+sudo gunicorn -D -b 0.0.0.0:8080 hello:app
+#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
+#sudo ln -sf /home/box/web/hello.py  /usr/lib/python2.7/dist-packages/gunicorn/app/hello.py
+#sudo /etc/init.d/gunicorn restart
