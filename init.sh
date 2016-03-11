c7acead5984a077957c74d10a371f75971d3ce66
AlxndrBo <sasha547@ya.ru>
2016-03-11 23:55:01 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ git config --global user.name "AlxndrBo"
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+#=======================================================
 cd /home/box/web
 sudo gunicorn -D -b 0.0.0.0:8080 hello:app
 cd /home/box/web/ask
@@ -10,3 +11,9 @@ sudo gunicorn -D -b 0.0.0.0:8000 ask.wsgi
 #sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
 #sudo ln -sf /home/box/web/hello.py  /usr/lib/python2.7/dist-packages/gunicorn/app/hello.py
 #sudo /etc/init.d/gunicorn restart
+#=======================================================
+mysql -u root -p
+create user 'pupkin'@'localhost' identified by 'pupkinpwd';
+create database askdb;
+grant all on askdb.* to 'test'@'localhost';
+#=======================================================
