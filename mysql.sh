3f35e8640b282fcec55be075c878f07eec9e95e4
AlxndrBo <sasha547@ya.ru>
2016-03-12 00:15:25 +0300

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,4 @@
 mysql -u root -p
-create user 'pupkin'@'localhost' identified by 'pupkinpwd';
-create database askdb;
-grant all on askdb.* to 'test'@'localhost';
+mysql -u root -p create user 'pupkin'@'localhost' identified by 'pupkinpwd';
+mysql -u root -p create database askdb;
+mysql -u root -p grant all on askdb.* to 'test'@'localhost';
